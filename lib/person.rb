99a1059a0ee10a1b09bfa0c9fe46701660e2f1ea
Tom White <eugene.white@gmail.com>
2019-06-17 14:00:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,7 @@ class Person
   
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(arguments)
+  def initialize(atributes)
+    atributes.each 
   
 end
